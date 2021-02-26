b133272ee760540c2a1eb40ef183ae193b396ef3
devandong <devandong@tencent.com>
2021-02-26 16:14:09 +0800

    update benchmark_ios, support set input and dump output

--- benchmark/benchmark_ios/benchmark/BenchmarkController.mm
+++ benchmark/benchmark_ios/benchmark/BenchmarkController.mm
@@ -19,6 +19,7 @@
 #include <sys/time.h>
 #include <float.h>
 #include <sstream>
+#include <map>
 
 using namespace std;
 using namespace TNN_NS;
@@ -79,6 +80,163 @@ struct BenchResult {
         return ostr.str();
     };
 };
+// save files on device
+void SaveMatToFile(Mat &mat, const std::string &filename) {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:[NSString stringWithCString:filename.c_str()
+                                                                                               encoding:[NSString defaultCStringEncoding]]];
+    std::stringstream ss;
+    const auto &dims = mat.GetDims();
+    // shape
+    for(int idx = 0; idx < dims.size(); ++idx) {
+        ss << dims[idx];
+        if (idx != dims.size()-1)
+            ss << ",";
+        else
+            ss << endl;
+    }
+    // data
+    const float *data = reinterpret_cast<float *>(mat.GetData());
+    for(int idx=0; idx < DimsVectorUtils::Count(dims); ++idx) {
+        ss << data[idx] << endl;
+    }
+    
+    NSString *content = [NSString stringWithCString:ss.str().c_str() encoding:[NSString defaultCStringEncoding]];
+    [content writeToFile:filePath atomically:NO
+                encoding:[NSString defaultCStringEncoding]
+                   error:nil];
+}
+
+using MatMap = std::map<std::string, std::shared_ptr<Mat>>;
+// create Mat according to blob
+MatMap CreateMatMap(BlobMap &blob_map, DeviceType device = DEVICE_ARM) {
+    MatMap mat_map;
+    for(auto &iter : blob_map) {
+        const auto &name = iter.first;
+        Blob *device_blob = iter.second;
+        BlobDesc &blob_desc = device_blob->GetBlobDesc();
+        
+        //DataType data_type = DATA_TYPE_FLOAT;
+        MatType mat_type   = NCHW_FLOAT;
+        
+        //size_t bytes = DimsVectorUtils::Count(blob_desc.dims) * DataTypeUtils::GetBytesSize(data_type);
+        auto mat = std::make_shared<Mat>(device, mat_type, blob_desc.dims);
+        mat_map[name] = mat;
+    }
+    return mat_map;
+}
+
+// Init MatMap
+bool InitMatMap(MatMap &mat_map, bool save_mat, const std::vector<std::string> &filepath_list) {
+    if (filepath_list.size()!=0 && mat_map.size() != filepath_list.size())
+        return false;
+    
+    bool use_file = filepath_list.size() != 0;
+    
+    int idx = 0;
+    for(auto iter=mat_map.begin(); iter!=mat_map.end(); ++iter, ++idx) {
+        const auto &name = iter->first;
+        auto mat = iter->second;
+        if (mat->GetMatType() != NCHW_FLOAT)
+            return false;
+        
+        auto data_count = DimsVectorUtils::Count(mat->GetDims());
+        void *mat_data  = mat->GetData();
+        if (use_file) {
+            std::ifstream input_stream(filepath_list[idx]);
+            if (!input_stream)
+                return false;
+            for(auto i=0; i<data_count; ++i) {
+                input_stream >> reinterpret_cast<float *>(mat_data)[i];
+            }
+        } else {
+            srand(time(nullptr));
+            for(auto i=0; i<data_count; ++i) {
+                reinterpret_cast<float *>(mat_data)[i] = static_cast<float>((rand() % 256 - 128)) / 128.0;
+            }
+        }
+    }
+    
+    if (save_mat && !use_file) {
+        for(auto iter=mat_map.begin(); iter != mat_map.end(); ++iter) {
+            const auto &name = iter->first;
+            auto mat         = iter->second;
+            SaveMatToFile(*mat, name+".txt");
+        }
+    }
+    
+    return true;
+}
+
+using BlobConverterMap = std::map<std::string, std::shared_ptr<BlobConverter>>;
+BlobConverterMap CreateBlobConverterMap(BlobMap &blob_map) {
+    std::map<std::string, std::shared_ptr<BlobConverter>> converter_map;
+    for(auto &iter : blob_map) {
+        auto blob_converter = std::make_shared<BlobConverter>(iter.second);
+        converter_map[iter.first] = blob_converter;
+    }
+    
+    return converter_map;
+}
+
+bool SetInputBlobs(Instance *instance, bool save_input=false, const std::vector<std::string> &filepath_list={}) {
+    BlobMap input_blob_map;
+    auto status = instance->GetAllInputBlobs(input_blob_map);
+    RETURN_VALUE_ON_NEQ(status, TNN_OK, false);
+    
+    MatMap input_mat_map = CreateMatMap(input_blob_map);
+    if (!InitMatMap(input_mat_map, save_input, filepath_list))
+        return false;
+    
+    BlobConverterMap blob_converter_map = CreateBlobConverterMap(input_blob_map);
+    
+    void *command_queue;
+    status = instance->GetCommandQueue(&command_queue);
+    RETURN_VALUE_ON_NEQ(status, TNN_OK, false);
+    
+    for(auto &converter : blob_converter_map) {
+        const auto &name = converter.first;
+        auto blob_converter = converter.second;
+        status = blob_converter->ConvertFromMat(*input_mat_map[name], MatConvertParam(), command_queue);
+        RETURN_VALUE_ON_NEQ(status, TNN_OK, false);
+    }
+    
+    return true;
+}
+
+MatMap GetOutputBlobs(Instance *instance, const std::vector<std::string> &filepath_list={}) {
+    BlobMap output_blob_map;
+    auto status = instance->GetAllOutputBlobs(output_blob_map);
+    RETURN_VALUE_ON_NEQ(status, TNN_OK, MatMap());
+    
+    MatMap output_map_map = CreateMatMap(output_blob_map);
+    
+    BlobConverterMap blob_converter_map = CreateBlobConverterMap(output_blob_map);
+    
+    void *command_queue;
+    status = instance->GetCommandQueue(&command_queue);
+    RETURN_VALUE_ON_NEQ(status, TNN_OK, MatMap());
+    
+    for(auto &converter : blob_converter_map) {
+        const auto &name = converter.first;
+        auto blob_converter = converter.second;
+        status = blob_converter->ConvertToMat(*output_map_map[name], MatConvertParam(), command_queue);
+        RETURN_VALUE_ON_NEQ(status, TNN_OK, MatMap());
+    }
+    
+    return output_map_map;
+}
+
+bool SaveOutputMatMap(MatMap &mat_map) {
+    for(auto &iter : mat_map) {
+        const auto &name = iter.first;
+        auto &mat = iter.second;
+        SaveMatToFile(*mat, name+".txt");
+    }
+    
+    return true;
+}
 
 @interface BenchmarkController () {
 }
@@ -96,7 +254,7 @@ struct BenchResult {
 }
 
 - (vector<BenchModel>)getAllModels {
-    NSString *modelZone = [[NSBundle mainBundle] pathForResource:@"model"
+    NSString *modelZone = [[NSBundle mainBundle] pathForResource:@"align"
                                                           ofType:nil];
     NSArray *modelList = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:modelZone
                                                                              error:nil];
@@ -159,8 +317,8 @@ struct BenchResult {
     auto allModels = [self getAllModels];
     
     BenchOption option;
-    option.warm_count = 5;
-    option.forward_count = 10;
+    option.warm_count = 0;
+    option.forward_count = 1;
     option.create_count = 1;
     
     //Get metallib path from app bundle
@@ -174,7 +332,7 @@ struct BenchResult {
     for (auto model : allModels) {
         NSLog(@"model: %s", model.name.c_str());
         allResult = [allResult stringByAppendingFormat:@"model: %s\n", model.name.c_str()];
-        
+        /*
         //benchmark on arm cpu
         auto result_arm = [self benchmarkWithProtoContent:model.tnn_proto_content
                                                 model:model.tnn_model_content
@@ -186,7 +344,7 @@ struct BenchResult {
         NSLog(@"arm: \ntime: %s", result_arm.description().c_str());
         allResult = [allResult stringByAppendingFormat:@"arm: \ntime: %s",
                      result_arm.description().c_str()];
-        
+        */
         
         //benchmark on gpu
         auto result_gpu = [self benchmarkWithProtoContent:model.tnn_proto_content
@@ -254,6 +412,12 @@ struct BenchResult {
         }
     }
     
+    // set input
+    if(!SetInputBlobs(instance.get(), true)){
+        result.status = Status(TNNERR_PARAM_ERR, "Set input blobs error!");
+        return result;
+    }
+    
     //warm cpu, only used when benchmark
     for (int cc=0; cc<option.warm_count; cc++) {
         result.status = instance->Forward();
@@ -286,6 +450,18 @@ struct BenchResult {
         instance->FinishProfile(true);
     }
 #endif
+    
+    // get output
+    MatMap output_mat_map = GetOutputBlobs(instance.get());
+    if (output_mat_map.size() <= 0) {
+        result.status = Status(TNNERR_PARAM_ERR, "Get output blobs error!");
+        return result;
+    }
+    
+    if (!SaveOutputMatMap(output_mat_map)) {
+        result.status = Status(TNNERR_PARAM_ERR, "Save output blobs error!");
+        return result;
+    }
 
     return result;
 }
